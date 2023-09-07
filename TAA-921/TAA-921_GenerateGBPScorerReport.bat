ff477626367b51a7821bdddfcaf2db8b2fb1c2ab
Jumer Acosta <jumer@axadra.com>
2023-09-07 12:47:04 +0800

    Updated the .bat with the correct path for .csv and html reports

--- TAA-921/TAA-921_GenerateGBPScorerReport.bat
+++ TAA-921/TAA-921_GenerateGBPScorerReport.bat
@@ -1,2 +1,2 @@
-cd C:\Program Files\apache-jmeter-5.5\bin
-jmeter -n -t "C:\Users\RND\Desktop\Perf(Auto) Test\Machine1\TAA-922\TAA-922_GenerateWebAuditReport.jmx" -l "C:\Users\RND\Desktop\Perf(Auto) Test\Machine1\TAA-922\CSV_Report_KEY1_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%\TAA-922_KEY1.csv" -e -o "C:\Users\RND\Desktop\Perf(Auto) Test\Machine1\TAA-922\HTML_Report_KEY1"
+cd C:\Program Files\apache-jmeter-5.6.2\bin
+jmeter -n -t "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-921\TAA-921_GenerateGBPScorerReport.jmx" -l "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-921\CSV_Report_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%\TAA-921_KEY1.csv" -e -o "C:\xampp\htdocs\PerformanceProject\WebAudit_TAA-921\HTML_Report_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%"
