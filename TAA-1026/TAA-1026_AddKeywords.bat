f8261fe049b04292d8d5b1d59f268891579dda58
Jumer Acosta <jumer@axadra.com>
2024-02-23 12:19:41 +0800

    Add keywords perf test added failed test in slack notif

--- TAA-1026/TAA-1026_AddKeywords.bat
+++ TAA-1026/TAA-1026_AddKeywords.bat
@@ -1,2 +1,4 @@
 cd C:\Program Files\apache-jmeter-5.6.2\bin
-jmeter -n -t "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-1026\TAA-1026_AddKeywords.jmx" -l "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-1026\CSV_Report_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%\TAA-1026_KEY1.csv" -e -o "C:\xampp\htdocs\PerformanceProject\AddKeywords_TAA-1026\HTML_Report_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%"
+jmeter -n -t "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-1026\TAA-1026_AddKeywords.jmx" -l "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-1026\CSV_Report_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%\TAA-1026_KEY1.csv" -e -o "C:\xampp\htdocs\PerformanceProject\WebAudit_TAA-1026\HTML_Report_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%"
+cd C:\Program Files\apache-jmeter-5.6.2\bin
+jmeter -n -t "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-1026\TAA-1026_AddKeywordsNotif.jmx"
