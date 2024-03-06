70911bb009bcc26c2efdb60e6fa47c25f8827f14
Jumer Acosta <jumer@axadra.com>
2024-03-06 12:01:41 +0800

    Add Campaign perf test is added

---
+++ TAA-1073/TAA-1073_AddCampaign.bat
@@ -0,0 +1,4 @@
+cd C:\Program Files\apache-jmeter-5.6.2\bin
+jmeter -n -t "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-1073\TAA-1073_AddCampaign.jmx" -l "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-1073\CSV_Report_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%\TAA-1073_KEY1.csv" -e -o "C:\xampp\htdocs\PerformanceProject\AddCampaign_TAA-1073\HTML_Report_%date:~-4,4%%date:~-10,2%%date:~3,2%_%time:~0,2%%time:~3,2%"
+cd C:\Program Files\apache-jmeter-5.6.2\bin
+jmeter -n -t "C:\Users\Admin\Desktop\Test Automation\Perf Test\Axadra.TestPerformance\TAA-1073\TAA-1073_AddCampaignNotif.jmx"
